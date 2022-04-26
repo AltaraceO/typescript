c31aaae1a624478f3c2f254c778e675fcdbb7979
AltaraceO <orialtarace@gmail.com>
2022-04-26 18:38:32 +0300

    setup, interface and React.FC<>

---
+++ rts/src/props/Child.tsx
@@ -0,0 +1,18 @@
+interface ChildProps {
+  color: string;
+  onClick: () => void;
+}
+
+// export const Child = ({ color }: ChildProps) => {
+//   return <div> {color}</div>;
+// };
+
+export const Child: React.FC<ChildProps> = ({ color, onClick }) => {
+  return (
+    <div>
+      {" "}
+      {color}
+      <button onClick={onClick}>Click Me</button>
+    </div>
+  );
+};
